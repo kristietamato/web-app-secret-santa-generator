8db22c7017fbf26236f9f996f4720d62e39d3a36
Kristie Nguyen <kristietamato@gmail.com>
2017-10-28 23:53:57 -0700

    Add initial modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -164,13 +164,12 @@
           swap(membersArrayShuffled, 0, 1);
         }
       }
-
       // Set member secret santa and giftee indexes
       membersArray[count2].memberSecretSantaIndex = membersArrayShuffled[0].memberIndex;
       // Pop the first item in shuffled arrays
       membersArrayShuffled.splice(0, 1);
     }
-    console.log(membersArray);
+    return openModal();
   }
 
   // https://git.daplie.com/Daplie/knuth-shuffle
@@ -198,6 +197,29 @@
 
     input[index0] = input[index1];
     input[index1] = temp;
+  }
+
+  function openModal() {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
   }
 })();
 
