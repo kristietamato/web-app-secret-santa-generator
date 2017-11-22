ec2f87f47bf88a4a96a63a515e5d0023ae0fb3aa
Kristie Nguyen <kristietamato@gmail.com>
2017-11-22 15:42:44 -0800

    Hide send email article

--- app/scripts/functionsLibrary.js
+++ app/scripts/functionsLibrary.js
@@ -34,30 +34,16 @@ function drawSecretSantas() {
 }
 
 function showSendEmail(membersArray) {
-  // Get the modal
-  var modal = document.getElementById('my-modal');
-  // Open the modal 
-  modal.style.display = 'block';
+  // Get the content
+  var content = document.getElementById('article-send-email');
+  // Open the content 
+  content.style.display = 'block';
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName('close')[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = 'none';
-  };
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  };
-
-  document.getElementById('email-all-members').addEventListener('click', sendDataToServer(membersArray));
+  document.getElementById('email-all-members').addEventListener('click', sendDataToServer(membersArray, content));
 }
 
-function sendDataToServer(membersArray) {
+function sendDataToServer(membersArray, content) {
+  content.style.display = 'none';
   var membersJSON = JSON.stringify(membersArray);
   $.ajax({
     type: 'POST',
